9c97d217f0872cd836aab1c727afa06a021d36b7
A01Mlner <milneradam0@gmail.com>
2024-11-01 11:17:08 +0000

    ADD: added framework for all tests

---
+++ BoardGame/src/test/java/TestBoardClass.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBoardClass {
+    @Test
+    public void LoseLifePass(){
+
+    }
+
+    @Test
+    public void LoseLifeGameOver(){
+
+    }
+
+    @Test
+    public void PiecesOnSameSquareFail(){
+
+    }
+
+    @Test
+    public void LevelPassed(){
+
+    }
+
+    @Test
+    public void DistanceToTreasure(){
+
+    }
+    @Test
+    public void BoardSetup(){
+
+    }
+
+}
---
+++ BoardGame/src/test/java/TestHeroClass.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHeroClass {
+    @Test
+    public void moveUpPass(){
+
+    }
+
+    @Test
+    public void moveUpOOB(){
+
+    }
+
+    @Test
+    public void moveLeftPass(){
+
+    }
+
+    @Test
+    public void moveLeftOOB(){
+
+    }
+
+    @Test
+    public void moveRightPass(){
+
+    }
+
+    @Test
+    public void moveRightOOB(){
+
+    }
+
+    @Test
+    public void moveDownPass(){
+
+    }
+
+    @Test
+    public void moveDownOOB(){
+
+    }
+
+    @Test
+    public void FindsMonster(){
+
+    }
+
+
+
+    @Test
+    public void CheckRandomPosition(){
+
+    }
+}
