2253192799c240f7b1ef344ebef21d4290471e8a
A01Mlner <milneradam0@gmail.com>
2024-11-01 11:55:29 +0000

    EDIT: added specific tests for the HERO

---
+++ BoardGame/src/main/java/com/lbg/Piece.java
@@ -0,0 +1,32 @@
+package com.lbg;
+import java.util.Random;
+
+public  class Piece {
+
+    private int[] position = new int[2]; //[xpos,ypos]
+
+    protected int[] genRandomPosition(int[] BoardSize) {
+        Random random = new Random();
+        int[] newPosition = new int[2];
+        for(int i =0;i<2;i++){
+            newPosition[i] = random.nextInt(BoardSize[i]); //gen a random number between 0 (inclusive) and BoardSize
+        }
+        return newPosition;
+    }
+
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Piece: {X-position: ").append(position[0]).append(", Y-position: ").append(position[1]).append("}");
+        return sb.toString();
+    }
+}
--- BoardGame/src/test/java/TestHeroClass.java
+++ BoardGame/src/test/java/TestHeroClass.java
@@ -1,44 +1,91 @@
+import com.lbg.Hero;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestHeroClass {
     @Test
     public void moveUpPass(){
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{0, 0});
+        h.moveUp();
+        assertEquals(0, h.getPosition[0]);
+        assertEquals(1, h.getPosition[1]);
 
     }
 
     @Test
     public void moveUpOOB(){
-
+        Hero h = new Hero();
+        h.setPosition(new int[]{0, 2});
+        Board b = new Board(new int[]{2, 2});
+        h.moveUp();
+        assertEquals(0, h.getPosition[0]);
+        assertEquals(2, h.getPosition[1]);
     }
 
     @Test
     public void moveLeftPass(){
-
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{2, 0});
+        h.moveLeft();
+        assertEquals(1, h.getPosition[0]);
+        assertEquals(0, h.getPosition[1]);
     }
 
     @Test
     public void moveLeftOOB(){
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{0, 0});
+        h.moveLeft();
+        assertEquals(0, h.getPosition[0]);
+        assertEquals(0, h.getPosition[1]);
 
     }
 
     @Test
     public void moveRightPass(){
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{0, 0});
+        h.moveRight();
+        assertEquals(1, h.getPosition[0]);
+        assertEquals(0, h.getPosition[1]);
 
     }
 
     @Test
     public void moveRightOOB(){
-
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{2, 0});
+        h.moveRight();
+        assertEquals(2, h.getPosition[0]);
+        assertEquals(0, h.getPosition[1]);
     }
 
     @Test
     public void moveDownPass(){
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{0, 2});
+        h.moveDown();
+        assertEquals(0, h.getPosition[0]);
+        assertEquals(1, h.getPosition[1]);
 
     }
 
     @Test
     public void moveDownOOB(){
+        Hero h = new Hero();
+        Board b = new Board(new int[]{2, 2});
+        h.setPosition(new int[]{0, 0});
+        h.moveDown();
+        assertEquals(0, h.getPosition[0]);
+        assertEquals(0, h.getPosition[1]);
 
     }
 
@@ -47,10 +94,20 @@ public class TestHeroClass {
 
     }
 
-
-
     @Test
     public void CheckRandomPosition(){
+        Board b = new Board(new int[]{10, 10});
+        Hero h = new Hero();
+        int[][] positions = new int[10][2];
+        for(int i=0;i<10;i++){
+            int[] pos = h.generateRandomPosition(b.boardSize);
+            positions[i][0]=pos[0];
+            assertTrue(pos[0]<b.getboardSize()[0] && pos[0]>=0);
+
+            positions[i][1]=pos[1];
+            assertTrue(pos[1]<b.getboardSize()[1] && pos[1]>=0);
+        }
+        
 
     }
 }
